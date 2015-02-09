ff2cf95d235141ca9212b33ea7dbcd69cfee15f6
robinmin <minlongbing@gmail.com>
2015-02-09 19:15:08 +0800

    Add unit test

--- sys/controllers/swale_tests.php
+++ sys/controllers/swale_tests.php
@@ -1,11 +1,14 @@
 <?php
 
+require SYS_PATH.'/controllers/utest.php';
+
 class swale_tests extends utest{
     public function __construct($name=''){
         parent::__construct($name);
     }
 
-    public function index(){
+    public function test_func1(){
+        $this->_assert_true(1==1);
         echo __METHOD__;
     }
 }
